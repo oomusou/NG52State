4fcd7790e71c476d29a30cf0a5c7946eabf2531a
Oomusou <oomusou@gmail.com>
2018-03-06 23:38:28 +0800

    Refactoring else

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,38 +13,50 @@ export class AppComponent {
     if (this.state === 'Locked') {
       this.state = 'Unlocked';
       this.message = 'The phone is unlocked';
-    } else if (this.state === 'Unlocked') {
-      this.state = 'Home';
-      this.message = 'The phone is at home';
-    } else if (this.state === 'App') {
-      this.state = 'Home';
-      this.message = 'The phone is at home';
-    } else if (this.state === 'Desktop') {
-      this.state = 'Home';
-      this.message = 'The phone is at home';
-    } else {
-      this.state = 'Home';
-      this.message = 'The phone is at home';
+      return;
     }
+
+    if (this.state === 'Unlocked') {
+      this.state = 'Home';
+      this.message = 'The phone is at home';
+      return;
+    }
+
+    if (this.state === 'App') {
+      this.state = 'Home';
+      this.message = 'The phone is at home';
+      return;
+    }
+
+    if (this.state === 'Desktop') {
+      this.state = 'Home';
+      this.message = 'The phone is at home';
+      return;
+    }
+
+    this.state = 'Home';
+    this.message = 'The phone is at home';
   }
 
   onOpenAppClick() {
     if (this.state === 'Home' || this.state === 'Desktop') {
       this.state = 'App';
       this.message = 'The phone is opening app';
-    } else {
-      this.state = 'Null';
-      this.message = 'The operation is not allowed';
+      return;
     }
+
+    this.state = 'Null';
+    this.message = 'The operation is not allowed';
   }
 
   onSwitchDesktopClick() {
     if (this.state === 'Home') {
       this.state = 'Desktop';
       this.message = 'The phone is switching desktop';
-    } else {
-      this.state = 'Null';
-      this.message = 'The operation is not allowed';
+      return;
     }
+
+    this.state = 'Null';
+    this.message = 'The operation is not allowed';
   }
 }
